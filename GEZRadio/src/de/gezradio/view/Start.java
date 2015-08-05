2a1b784e1d2f2960d160006485e5dc58c6fcdf40
Ole Goes <o.goes@ostfalia.de>
2015-08-06 01:21:33 +0200

    Warnings weggemacht

--- GEZRadio/src/de/gezradio/view/Start.java
+++ GEZRadio/src/de/gezradio/view/Start.java
@@ -9,6 +9,10 @@ import javax.swing.border.EmptyBorder;
 
 public class Start extends JFrame {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5468153855703685352L;
 	private JPanel contentPane;
 
 	/**
